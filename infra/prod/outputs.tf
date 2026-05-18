4f4edfe91ed260116f34142aea4c9ed65d9d288a
Juan E. Arango Z <juarangoz@unal.edu.co>
2026-05-18 14:21:53 -0500

    feat(website): add Amplify deployment config and Terraform IaC

--- infra/prod/outputs.tf
+++ infra/prod/outputs.tf
@@ -12,3 +12,18 @@ output "lambda_role_arn" {
   description = "IAM role ARN for CRUD Lambdas."
   value       = module.crud.lambda_role_arn
 }
+
+output "api_endpoint" {
+  description = "API Gateway HTTP endpoint URL."
+  value       = module.crud.api_endpoint
+}
+
+output "amplify_app_id" {
+  description = "AWS Amplify application ID."
+  value       = aws_amplify_app.website.id
+}
+
+output "amplify_branch_url" {
+  description = "AWS Amplify branch URL for the deployed website."
+  value       = aws_amplify_branch.website_main.branch_url
+}
